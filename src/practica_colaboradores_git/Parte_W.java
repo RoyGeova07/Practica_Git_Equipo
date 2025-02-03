312f0f6d6d8b7c9feab2b0a983efd52cfcfd94a1
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-02-03 13:06:36 -0600

    hola soy colaborador

---
+++ src/practica_colaboradores_git/Parte_W.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica_colaboradores_git;
+
+/**
+ *
+ * @author DELL
+ */
+public class Parte_W {
+    public static void main(String[] args) {
+        System.out.println("Aporte grandisimo de william");
+    }
+}
